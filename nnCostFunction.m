48a33a257b05272e882634ff10ac89ef9e278e4e
tinf <ericding1213@gmail.com>
2022-05-14 10:48:10 +0800

    fix nnCost

--- nnCostFunction.m
+++ nnCostFunction.m
@@ -97,7 +97,6 @@ Theta1_grad = Theta1_grad * (1 / m);
 Theta2_grad = Theta2_grad * (1 / m);
 Theta2_grad(:, 2:end) = Theta2_grad(:, 2:end) + (lambda / m) * Theta2(:, 2:end);
 Theta1_grad(:, 2:end) = Theta1_grad(:, 2:end) + (lambda / m) * Theta1(:, 2:end);
-whos;
 
 % -------------------------------------------------------------
 
