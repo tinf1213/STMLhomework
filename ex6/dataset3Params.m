0ffd5b56a7ed116e8d0dc08b0632cfc8db5fdbe0
tinf1213 <tinf@DESKTOP-89QRF1N.localdomain>
2022-05-22 21:41:01 +0800

    complete gaussian and data3

--- ex6/dataset3Params.m
+++ ex6/dataset3Params.m
@@ -23,11 +23,26 @@ sigma = 0.3;
 %        mean(double(predictions ~= yval))
 %
 
-
-
-
-
-
+tar = [0.01; 0.03; 0.1; 0.3; 1; 3; 10; 30];
+mini = 9999999999;
+i_mark = 0;
+j_mark = 0;
+for i = 1:8,
+	C = tar(i);
+	for j = 1:8,
+		sigma = tar(j);
+		model = svmTrain(X, y, C, @(x1, x2) gaussianKernel(x1, x2, sigma));
+		pred = svmPredict(model, Xval);
+		err = mean(double(pred ~= yval));
+		if (err < mini),
+			i_mark = i;
+			j_mark = j;
+			mini = err;
+		endif
+	end
+end
+C = tar(i_mark);
+sigma = tar(j_mark);
 
 % =========================================================================
 
